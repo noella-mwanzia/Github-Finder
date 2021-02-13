64f0bcd401899ae76ee27ccf0b213dcca6511b15
noellamwanzia <noellamwanzia@gmail.com>
2021-02-13 10:00:31 +0300

    first commit

---
+++ github.js
@@ -0,0 +1,21 @@
+class Github {
+    constructor() {
+        this.client_id = '20b943e85865bfea0ddc';
+        this.client_secret = 'ce2f839934b02f108b5f3c3c19a49573385758bd';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
